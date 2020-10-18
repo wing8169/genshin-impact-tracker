4e25cbf5b5084b639d534cd8ae407345a81a5d1e
Chin Jia Xiong <chinxiongwei@yahoo.com.my>
2020-10-18 15:57:01 +0800

    Load Teyvat

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,32 +1,38 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { PictureInPictureMagnifier } from "react-image-magnifiers";
 import teyvat from "../../images/teyvat.png";
+import { Map, ImageOverlay } from "react-leaflet";
+import { CRS, LatLngBounds, LatLng } from "leaflet";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
+  background: {
     width: "100vw",
     height: "100vh",
-    padding: 0,
-  },
-  background: {
-    width: 600,
-    margin: "auto",
+    overflow: "hidden",
   },
 }));
 
-const Map = () => {
+const MyMap = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <PictureInPictureMagnifier
-        imageSrc={teyvat}
-        imageAlt="Teyvat"
-        className={classes.background}
+    <Map
+      center={[3000, 3000]}
+      zoom={0}
+      maxZoom={0.5}
+      minZoom={-2}
+      className={classes.background}
+      crs={CRS.Simple}
+      bounds={new LatLngBounds(new LatLng(0, 6144), new LatLng(6144, 0))}
+      maxBounds={new LatLngBounds(new LatLng(0, 6144), new LatLng(6144, 0))}
+    >
+      <ImageOverlay
+        url={teyvat}
+        bounds={new LatLngBounds(new LatLng(0, 6144), new LatLng(6144, 0))}
+        maxBounds={new LatLngBounds(new LatLng(0, 6144), new LatLng(6144, 0))}
       />
-    </div>
+    </Map>
   );
 };
 
-export default Map;
+export default MyMap;
