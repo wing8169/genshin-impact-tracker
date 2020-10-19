c65a1310c1cc5e266fdf051a7ff96317478cb5ff
Chin Jia Xiong <chinxiongwei@yahoo.com.my>
2020-10-19 14:32:23 +0800

    Added Tutorial Contents

--- src/components/tutorial/tutorial.js
+++ src/components/tutorial/tutorial.js
@@ -42,14 +42,26 @@ const Tutorial = () => {
             ref={descriptionElementRef}
             tabIndex={-1}
           >
-            {[...new Array(50)]
-              .map(
-                () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-              )
-              .join("\n")}
+            Genshin Impact Tracker is a personal research project to study the
+            behavior of resource respawn time. <br />
+            <br />
+            Via this platform, you can navigate through the world of Teyvat and
+            click to create a new marker. Once you have found the resources
+            again, click on the existing markers and click "Found Again". <br />
+            <br />
+            Estimation of respawn time will be provided based on the shortest
+            respawn interval (By Hours). You can keep track of your activities
+            at the side menu, and click to see the markers involved. Besides,
+            you can click on the upper right notification icon button to see
+            what are the resources that are expected to be respawned (Will be
+            highlighted red). Take note that the estimation will only be carried
+            out if you have found a resource <b>at least twice.</b> <br />
+            <br />
+            <b>
+              The current version does not support cloud syncing, your data will
+              only be accessible to your device.{" "}
+            </b>
+            We are working on cloud syncing and will be up soon!
           </DialogContentText>
         </DialogContent>
         <DialogActions>
