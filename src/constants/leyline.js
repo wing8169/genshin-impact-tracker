6cc0d81ce2a423ac58cf27fd975017da6752b03b
Chin Jia Xiong <chinxiongwei@yahoo.com.my>
2020-11-02 19:03:26 +0800

    Implemented Materials and Leyline Calculation

---
+++ src/constants/leyline.js
@@ -0,0 +1,59 @@
+// reference: https://genshin-impact.fandom.com/wiki/Ley_Line_Outcrops
+export const leylineExpList = [
+  {
+    minAr: 8,
+    maxAr: 10,
+    small: 3,
+    medium: 7,
+    large: 0,
+  },
+  {
+    minAr: 12,
+    maxAr: 19,
+    small: 3,
+    medium: 7,
+    large: 0,
+  },
+  {
+    minAr: 20,
+    maxAr: 24,
+    small: 5,
+    medium: 10,
+    large: 0,
+  },
+  {
+    minAr: 25,
+    maxAr: 29,
+    small: 5,
+    medium: 12,
+    large: 0,
+  },
+  {
+    minAr: 30,
+    maxAr: 34,
+    small: 0,
+    medium: 13,
+    large: 0,
+  },
+  {
+    minAr: 35,
+    maxAr: 39,
+    small: 0,
+    medium: 6,
+    large: 2,
+  },
+  {
+    minAr: 40,
+    maxAr: 44,
+    small: 0,
+    medium: 6,
+    large: 3,
+  },
+  {
+    minAr: 45,
+    maxAr: 49,
+    small: 0,
+    medium: 6,
+    large: 4,
+  },
+];
